110b6055520bb95694df8d312cfc358b48ec02bc
miglio000 <gabrielemiglio23@gmail.com>
2021-03-02 16:14:11 +0100

    Add project files.

---
+++ CalcolatoreFrequenza/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using LibreriaFrequanzaElementi;
+namespace CalcolatoreFrequenza
+{
+    class Program
+    {
+        const int dimensione = 10;
+        static void Main(string[] args)
+        {
+            int[] array_numeri = new int[10];
+            int maggiore, volte_max = 1, attuale, volte_attuale;
+            Console.WriteLine("10 elementi: ");
+            Gestione.LeggiArray(array_numeri);
+            Gestione.BubbleSort(array_numeri);
+            maggiore = array_numeri[0];
+            for (int i = 0; i < array_numeri.Length; i++)
+            {
+                attuale = array_numeri[i];
+                volte_attuale = 1;
+                for (int j = i + 1; j < array_numeri.Length; j++)
+                {
+                    if (array_numeri[i] == array_numeri[j])
+                        volte_attuale++;
+                }
+                if (volte_attuale > volte_max)
+                {
+                    maggiore = attuale;
+                    volte_max = volte_attuale;
+                }
+            }
+            Console.WriteLine($"Il numero che compare piu' volte e' {maggiore} e si ripete {volte_max} volte");
+
+
+
+
+            /*int j = 0;
+            for (int i = 0; i < array_numeri.Length - 1; i++)
+            {
+                if (array_numeri[i] == array_numeri[i + 1])
+                {
+                    j++;
+                }
+            }
+            int[] array_frequenze = new int[j];
+            for (int i = 0, k = 0; i < array_numeri.Length - 1; i++)
+            {
+                if (array_numeri[i] == array_numeri[i + 1])
+                {
+                    array_frequenze[k] = array_numeri[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("Le frequenze sono: ");
+            Gestione.StampaArray(array_frequenze);*/
+        }
+    }
+}
---
+++ LibreriaFrequanzaElementi/Gestione.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LibreriaFrequanzaElementi
+{
+    public class Gestione
+    {
+        public static void LeggiArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                do
+                {
+                    try
+                    {
+                        array[i] = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (array[i] < int.MinValue || array[i] > int.MaxValue);
+            }
+        }
+        public static void BubbleSort(int[] n)
+        {
+            int x, y;
+            int temp;
+            for (x = 0; x < n.Length - 1; x++)
+            {
+                for (y = 0; y < n.Length - 1 - x; y++)
+                {
+                    if (n[y] > n[y + 1])
+                    {
+                        temp = n[y];
+                        n[y] = n[y + 1];
+                        n[y + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void StampaArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
